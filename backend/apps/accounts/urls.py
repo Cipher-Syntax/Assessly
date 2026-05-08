57771d93bdc51044db03b8dcbaf6c62251cddf18
Justine Toong <programmerjustine@gmail.com>
2026-05-08 12:59:11 +0800

    feat: add frontend api client

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path #type: ignore
 
 from apps.accounts.views import (
     GoogleLoginView,
-    LoginView,
     LogoutView,
     RegisterView,
     ResendOtpView,
@@ -13,7 +12,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="accounts-register"),
     path("verify-otp/", VerifyOtpView.as_view(), name="accounts-verify-otp"),
     path("resend-otp/", ResendOtpView.as_view(), name="accounts-resend-otp"),
-    path("login/", LoginView.as_view(), name="accounts-login"),
     path("logout/", LogoutView.as_view(), name="accounts-logout"),
     path("google/", GoogleLoginView.as_view(), name="accounts-google-login"),
 ]
